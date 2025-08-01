67ac542b782566bf73b3329d2f612e5af9ff1e9a
rojasadrian012 <rojasadrian012@gmail.com>
2025-08-01 11:11:14 -0300

    add new types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,19 @@
+import type { JSX } from "react";
+
 export interface IMenuItem {
     text: string;
     url: string;
+}
+
+export interface IBenefit {
+    title: string;
+    description: string;
+    imageSrc: string;
+    bullets: IBenefitBullet[]
+}
+
+export interface IBenefitBullet {
+    title: string;
+    description: string;
+    icon: JSX.Element;
 }
