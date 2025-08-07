cc3a443d614361eb7ce012c5cd684a02865367f5
rojasadrian012 <rojasadrian012@gmail.com>
2025-08-07 08:35:39 -0300

    Refactor TypeScript configuration and Vite setup to standardize path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,17 @@ export default defineConfig({
     tailwindcss(),
     react(),
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@assets': resolve(__dirname, 'src/assets'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@data': resolve(__dirname, 'src/data'),
+      '@hooks': resolve(__dirname, 'src/hooks'),
+      '@lib': resolve(__dirname, 'src/lib'),
+      '@pages': resolve(__dirname, 'src/pages'),
+      '@styles': resolve(__dirname, 'src/styles'),
+      '@types': resolve(__dirname, 'src/types'),
+    },
+  }
 })
