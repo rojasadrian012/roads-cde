fd1a1397fb522dabf7637a177ff1adbb2f3c8dee
rojasadrian012 <rojasadrian012@gmail.com>
2025-08-13 09:42:18 -0300

    change fallback size

--- src/components/ui/fallback.component.tsx
+++ src/components/ui/fallback.component.tsx
@@ -4,7 +4,7 @@ interface FallbackProps {
 
 export const Fallback: React.FC<FallbackProps> = ({ text }) => {
     return (
-        <div className="h-full flex flex-col justify-center items-center">
+        <div className="pt-24 min-h-screen flex flex-col justify-center items-center">
             <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary"></div>
             <p className="mt-4 text-primary text-2xl italic">{text}</p>
         </div>
